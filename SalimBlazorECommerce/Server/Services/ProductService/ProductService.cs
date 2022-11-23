81716d4c22e2b3e8af728d765bd80d6318d120ee
Salim <laimeche160@gmail.com>
2022-11-23 15:52:33 +0100

    type of product

--- SalimBlazorECommerce/Server/Services/ProductService/ProductService.cs
+++ SalimBlazorECommerce/Server/Services/ProductService/ProductService.cs
@@ -12,7 +12,7 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products.Include(p => p.Variants).ThenInclude(v => v.ProductType).FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
@@ -29,7 +29,7 @@
         {
             var response = new ServiceResponse<List<Product>>()
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -39,7 +39,7 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.Where(p => p.Category.Url.ToLower().Equals(categoryUrl)).ToListAsync()
+                Data = await _context.Products.Where(p => p.Category.Url.ToLower().Equals(categoryUrl)).Include(p => p.Variants).ToListAsync()
             };
 
             return response;
---
+++ SalimBlazorECommerce/Shared/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalimBlazorECommerce.Shared
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+    }
+}
